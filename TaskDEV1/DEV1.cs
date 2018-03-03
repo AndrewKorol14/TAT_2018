30907a15b9cdb9f8939d0770eb118651129b8d4e
AndrewKorol <korol.andrew14@gmail.com>
2018-03-03 14:27:16 +0300

    Add

--- TaskDEV1/DEV1.cs
+++ TaskDEV1/DEV1.cs
@@ -11,21 +11,13 @@ namespace TaskDEV1
     {
         static void Main(string[] args)
         {
-            try
+            if(args.Length != 1)
             {
-                if(args.Length != 1)
-                {
-                    throw new Exception("method must get only one string.");
-                }
-            }
-            catch (Exception exceptionArgument)
-            {
-                Console.WriteLine("Exception message: " + exceptionArgument.Message);
+                Console.WriteLine("Exception message: method must get only one string.");
                 return;
             }
-            var enteringString=args[0].ToCharArray();
             MaxNumberSerialSymbols EntryPointObject = new MaxNumberSerialSymbols();
-            Console.WriteLine("Max number of symbols: "+EntryPointObject.FindMaxNumberSymbols(enteringString));
+            Console.WriteLine("Max number of symbols: "+EntryPointObject.FindMaxNumberSymbols(args[0]));
         }
         /// <summary>
         /// Method FindMaxNumberSymbols
@@ -33,14 +25,14 @@ namespace TaskDEV1
         /// </summary>
         /// <param name="stringArgument">String, which was inputed</param>
         /// <returns>Maximum number of same serial symbols in the string</returns>
-        int FindMaxNumberSymbols(char[] stringArgument)
+        int FindMaxNumberSymbols(string stringArgument)
         {
             int maxNumber = 0;
             int tempMaxNumber = 0;
             for (int i = 0; i < stringArgument.Length - 1; i++)
             {
                 if (stringArgument[i] == stringArgument[i + 1])
-                {
+                {   
                     tempMaxNumber++;
                 }
                 else
