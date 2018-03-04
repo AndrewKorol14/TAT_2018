9d49b20ce4dab2b50a9a25edda7d01a9329a8c39
AndrewKorol <korol.andrew14@gmail.com>
2018-03-04 13:26:20 +0300

    Add DEV2

---
+++ TaskDEV2/EntryPoint.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TaskDEV2
+{
+    /// <summary>
+    /// Class EntryPoint
+    /// gets the string from console
+    /// </summary>
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            var enteredString = Console.ReadLine();
+            if (enteredString == "")
+            {
+                Console.WriteLine("Exception message: string hasn't any symbols.");
+                return;
+            }
+            EvenIndexSymbols EvenIndexString = new EvenIndexSymbols();
+            Console.WriteLine(EvenIndexString.DeleteSymbolsWithOddIndex(enteredString));
+        }
+    }
+}
