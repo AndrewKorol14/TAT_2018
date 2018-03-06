14d2d39017bd1f8d60aeba244fefc463ebdaa0ab
AndrewKorol <korol.andrew14@gmail.com>
2018-03-07 00:01:40 +0300

    Add task3

---
+++ TaskDEV3/EntryPoint.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace TaskDEV3
+{
+    /// <summary>
+    /// Class EntryPoint
+    /// must get two string arguments, else process exceptions
+    /// </summary>
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length!=2)
+            {
+                Console.WriteLine("Exception message: wrong number of arguments(should be two arguments).");
+                return;
+            }
+
+            if (args[0]==String.Empty||args[1]==String.Empty)
+            {
+                Console.WriteLine("Exception message: something string hasn't any symbols.");
+                return;
+            }
+
+            int checkingNumber;
+            if (!Int32.TryParse(args[0], out checkingNumber)||!Int32.TryParse(args[1], out checkingNumber))
+            {
+                Console.WriteLine("Exception message: string of decimal number or string of base new numeral system contains wrong symbols.");
+                return;
+            }
+
+            if (Convert.ToInt16(args[1])<2||Convert.ToInt16(args[1])>20)
+            {
+                Console.WriteLine("Exception message: wrong base of new numeral system.");
+                return;
+            }
+            NewNumeralSystemInterpreter newSystemNumber = new NewNumeralSystemInterpreter(Int32.Parse(args[0]), Int32.Parse(args[1]));
+            Console.Write("Number in new numeral system: ");
+            Console.WriteLine(newSystemNumber.ReverseNumberInNewSystem(newSystemNumber.CreateNumberInNewNumeralSystem()));
+        }
+    }
+}
---
+++ TaskDEV3/NewNumeralSystemInterpreter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Text;
+
+namespace TaskDEV3
+{
+    /// <summary>
+    /// Class NewNumeralSystemInterpreter
+    /// interprets decimal number in other numeral system
+    /// </summary>
+    public class NewNumeralSystemInterpreter
+    {
+        int numberInOldSystem;
+        int numeralSystemBase;
+
+        public NewNumeralSystemInterpreter(int oldSystemNumberArgument, int systemBaseArgument)
+        {
+            numberInOldSystem = oldSystemNumberArgument;
+            numeralSystemBase = systemBaseArgument;
+        }
+
+        /// <summary>
+        /// Method CreateNumberInNewNumeralSystem
+        /// represents decimal number in new numeral system
+        /// </summary>
+        /// <returns>Backward string of number in new system</returns>
+        public string CreateNumberInNewNumeralSystem()
+        {
+            StringBuilder newSystemNumberString = new StringBuilder();
+            int tempDivisionRemainder;
+            do
+            {
+                tempDivisionRemainder = numberInOldSystem % numeralSystemBase;
+                numberInOldSystem = numberInOldSystem / numeralSystemBase;
+                if (tempDivisionRemainder < 10)
+                {
+                    char tempCharSymbol = (char)(tempDivisionRemainder + 48); //using ASCII table, for choosing next numeral in the number of new numeral system
+                    newSystemNumberString.Append(tempCharSymbol.ToString());
+                }
+                else
+                {
+                    char tempCharSymbol = (char)(tempDivisionRemainder + 55); //using ASCII table, for choosing next numeral in the number of new numeral system
+                    newSystemNumberString.Append(tempCharSymbol.ToString());
+                }
+            } while (numberInOldSystem > 0);
+            return newSystemNumberString.ToString();
+        }
+
+        /// <summary>
+        /// Method ReverseNumberInNewSystem
+        /// reverses backward string
+        /// </summary>
+        /// <param name="stringArgument">Backward string</param>
+        /// <returns>Normal string</returns>
+        public string ReverseNumberInNewSystem(string stringArgument)
+        {
+            char[] charArrayForReverse=stringArgument.ToCharArray();
+            Array.Reverse(charArrayForReverse);
+            return new string(charArrayForReverse);
+        }
+    }
+}
