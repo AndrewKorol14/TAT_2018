9da448f0fa3e437cb74754ee19e950e56394a17f
AndrewKorol <korol.andrew14@gmail.com>
2018-03-23 16:57:38 +0300

    Correction 3

--- TaskDEV6/ProductCreator.cs
+++ TaskDEV6/ProductCreator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace TaskDEV6
 {
@@ -17,9 +18,15 @@ namespace TaskDEV6
             this.typeOfProduct = typeOfProduct;
             this.nameOfProduct = nameOfProduct;
             this.quantityOfProducts = Int32.Parse(quantityOfProducts);
-            if (this.quantityOfProducts < 0)
+            if (this.quantityOfProducts <= 0)
             {
                 throw new ArgumentException("Quantity of products must be positive");
+            }
+            char separatorOfRealValue = Convert.ToChar(Thread.CurrentThread.CurrentCulture.NumberFormat.NumberDecimalSeparator);
+            if (!Double.TryParse(priceOfProduct, out this.priceOfProduct))
+            {
+                priceOfProduct = priceOfProduct.Replace('.', separatorOfRealValue);
+                priceOfProduct = priceOfProduct.Replace(',', separatorOfRealValue);
             }
             this.priceOfProduct = Double.Parse(priceOfProduct);
             if (this.priceOfProduct <= 0.0)
