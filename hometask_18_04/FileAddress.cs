accc4302374a76c6bb82a58310a4577d9411ce02
AndrewKorol <korol.andrew14@gmail.com>
2018-04-19 23:14:34 +0300

    Hometask from 18.04

---
+++ hometask_18_04/FileAddress.cs
@@ -0,0 +1,27 @@
+﻿namespace hometask_18_04
+{
+    /// <summary>
+    /// Create address of file in internet
+    /// </summary>
+    class FileAddress
+    {
+        string address;
+        string fileName;
+
+        public FileAddress(string address, string fileName)
+        {
+            this.address = address;
+            this.fileName = fileName;
+        }
+
+        public string Address
+        {
+            get { return address; }
+        }
+
+        public string FileName
+        {
+            get { return fileName; }
+        }
+    }
+}
---
+++ hometask_18_04/UrlСontainer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace hometask_18_04
+{
+    /// <summary>
+    /// Create container of URL addresses and names of files
+    /// </summary>
+    class UrlСontainer
+    {
+        List<FileAddress> urls;
+        
+        public UrlСontainer()
+        {
+            urls = new List<FileAddress>();
+            urls.Add(new FileAddress("https://raw.githubusercontent.com/AndrewKorol14/TAT_2018/temp_for_hometask/files_for_downloading/dogAndWolf.jpg", "file1.jpg"));
+            urls.Add(new FileAddress("https://raw.githubusercontent.com/AndrewKorol14/TAT_2018/temp_for_hometask/files_for_downloading/dogAndWolf.jpg", "file2.jpg"));
+            urls.Add(new FileAddress("https://raw.githubusercontent.com/AndrewKorol14/TAT_2018/temp_for_hometask/files_for_downloading/C_6_0_Spravochnik_Polnoe_opisanie_yazyka_-_2016.pdf", "file3.pdf"));
+        }
+
+        public List<FileAddress> Urls
+        {
+            get { return urls; }
+        } 
+    }
+}
