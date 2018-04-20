67c3f2bdb95a5b67dd6a7fe91b55149de5a82b3b
AndrewKorol <korol.andrew14@gmail.com>
2018-04-20 10:24:08 +0300

    Task 19.04.2018

---
+++ task_19_04/EntryPoint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace task_19_04
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            СoordinatePair coordinates = new СoordinatePair(rnd.Next(1,10));
+            double finalResult = coordinates.DistanceCounter(СoordinatePair.GetDistanceByDecart); ;
+            Console.WriteLine("Distance in decart system: " + finalResult);
+            finalResult = coordinates.DistanceCounter(СoordinatePair.GetDistanceByOtherMethod); ;
+            Console.WriteLine("Distance in other system: " + finalResult);
+        }
+    }
+}
---
+++ task_19_04/PointCreator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace task_19_04
+{
+    class PointCreator
+    {
+        List<double> point;
+
+        public PointCreator()
+        {
+            point = new List<double>();
+        }
+
+        public List<double> Point
+        {
+            get { return point; }
+        }
+    }
+}
