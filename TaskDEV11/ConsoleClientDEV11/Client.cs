4b8cd3ad16869b97567a181b98c807e0443ec660
AndrewKorol <korol.andrew14@gmail.com>
2018-05-13 17:22:18 +0300

    Add DEV-11

---
+++ TaskDEV11/ConsoleClientDEV11/Client.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleClientDEV11
+{
+    /// <summary>
+    /// Creates a client of web service
+    /// </summary>
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                OurEraDayQuantityRef.GetterDaysService webService = new OurEraDayQuantityRef.GetterDaysService();
+                Console.WriteLine("Enter the date in format DD/MM/YYYY, DD-MM-YYYY, DD.MM.YYYY, DD Month YYYY...");
+                DateTime date = DateTime.Parse(Console.ReadLine());
+                Console.WriteLine("Quantity of days of our era: " + webService.GetDayQuantity(date));
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(WebException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ TaskDEV11/WebServiceDEV11/GetterDaysService.asmx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebServiceDEV11
+{
+    /// <summary>
+    /// Create web service for getting day quantity
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    public class GetterDaysService : System.Web.Services.WebService
+    {
+        /// <summary>
+        /// Gets quantity of days from the beginning of our era to inputted date
+        /// </summary>
+        /// <param name="date">Inputted date</param>
+        /// <returns>Day quantity</returns>
+        [WebMethod]
+        public uint GetDayQuantity(DateTime date)
+        {
+            DateTime christmasDate = DateTime.MinValue;
+            uint dayQuantity = (uint)(date - christmasDate).TotalDays;
+            return dayQuantity;
+        }
+    }
+}
