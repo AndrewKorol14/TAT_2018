d18651c0982b95581120986b551274edd58ecfa7
AndrewKorol <korol.andrew14@gmail.com>
2018-05-21 21:36:01 +0300

    Add Task3

---
+++ Task3/FilesFormatChecker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task3
+{
+    /// <summary>
+    /// Checker for checking of format correctness
+    /// </summary>
+    class FilesFormatChecker
+    {
+        /// <summary>
+        /// Converts the inputted format into correct format for function of searching
+        /// </summary>
+        /// <param name="inputFormat">Inputted format</param>
+        /// <returns>Correct format</returns>
+        public string GetTrueFormat(string inputFormat)
+        {
+            if (inputFormat == String.Empty)
+            {
+                inputFormat = "*.*";
+            }
+            if (!inputFormat.Contains("*"))
+            {
+                inputFormat = "*" + inputFormat;
+            }
+            return inputFormat;
+        }
+    }
+}
